8dfd01f6e38e33e9e55eb179fae5efa73b0c8e72
William Luck <luck.william.h@gmail.com>
2022-11-08 15:23:54 -0300

    commit tree d0d7af1b97b20708339b3a0b3aabda10b00e3333

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,25 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
+
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
+
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+
+    # cookies[:page_views] 
+    session[:page_views] ||= 0
+    session[:page_views] = session[:page_views].to_i + 1 
+
+
+    if session[:page_views] > 3
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    else
+      article = Article.find(params[:id])
+      render json: article
+    end
   end
 
   private
